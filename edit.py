f23c1936edf738842e83901a8648387e1dc76ffe
unknown <thukano0405@i.softbank.jp>
2021-07-12 07:48:33 +0900

    Create edit.py

--- edit.py
+++ edit.py
@@ -3,14 +3,22 @@ import openpyxl
 import os
 
 def edit(files):
-
+    print('sono1')
+    print(files)
     #パスを作成
     edit_file_path = 'download/' + 'test' + '_result.xlsx'
+    print('sono2')
+    print(edit_file_path)
     #作成したパスにエクセルファイルを保存
-    edit_df.to_excel(edit_file_path)
-
+    files[0].to_excel(edit_file_path)
+    print('sono3')
+    print(edit_file_path)
     workbook = openpyxl.load_workbook(edit_file_path)
+    print('sono4')
+    print(workbook)
     worksheet = workbook.worksheets[0]
+    print('sono5')
+    print(worksheet)
 
     #処理を保存
     workbook.save(edit_file_path)
