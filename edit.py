52172b3abe557ffa13b8cd22cbcc5a4c864b914e
unknown <thukano0405@i.softbank.jp>
2021-07-12 19:24:00 +0900

    Create edit.py

--- edit.py
+++ edit.py
@@ -5,19 +5,22 @@ import os
 def edit(files):
     print('sono1')
     print(files)
-    #パスを作成
-    edit_file_path = 'download/' + 'test' + '_result.xlsx'
+    #データフレーム形式に変換
+    edit_df = pd.read_excel('static/xlsx')
     print('sono2')
-    print(edit_file_path)
-    #作成したパスにエクセルファイルを保存
-    files[0].to_excel(edit_file_path)
+    print(edit_df)
+    #パスを作成
+    edit_file_path = 'download/' + 'static/xlsx' + '_result.xlsx'
     print('sono3')
     print(edit_file_path)
-    workbook = openpyxl.load_workbook(edit_file_path)
+    #作成したパスにエクセルファイルを保存
+    edit_df.to_excel(edit_file_path)
     print('sono4')
+    workbook = openpyxl.load_workbook(edit_file_path)
+    print('sono5')
     print(workbook)
     worksheet = workbook.worksheets[0]
-    print('sono5')
+    print('sono6')
     print(worksheet)
 
     #処理を保存
